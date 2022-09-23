c23afc69eabc7d7a36add772528df025c60e75cc
maui-dev <108886458+Jamesyun123@users.noreply.github.com>
2022-09-23 13:40:09 +0900

    addDecideWinner

--- src/page/Play.jsx
+++ src/page/Play.jsx
@@ -1,12 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Square from './Square';
+import decideWinner from './decideWinner';
 
 const Play = () => {
   const [values, setValues] = useState(Array(9).fill(null));  
   const [xIsNext, setxIsNext] = useState(true);
-
+  const [winner, setWinner] = useState(null);
+  
   const handleClick = (i) => {
     const temp = values.slice();
+    if (decideWinner(values) || values[i]) {
+      return;
+    }
     temp[i] = (xIsNext? "X" : "O");
     setValues(temp);
     setxIsNext(!xIsNext);
@@ -17,11 +22,20 @@ const Play = () => {
     squares.push(<Square value={values[i]} handleClick={() => handleClick(i)}/>)
   }
 
+  useEffect(() => {
+    setWinner(decideWinner(values));
+  },[values])
+
   return(
     <div className="flex flex-col items-center">
-      <div className="text-3xl mb-5">
-        Next Player:{xIsNext ? 'X' : 'O'}
-      </div>
+      {winner ? 
+        <div className="text-3xl mb-5">
+          winner is:{winner}
+        </div> :
+        <div className="text-3xl mb-5">
+          Next Player:{xIsNext ? 'X' : 'O'}
+        </div>
+      }
       <div className="grid grid-cols-3">
         {squares}
       </div>
